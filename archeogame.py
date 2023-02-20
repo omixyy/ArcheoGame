182fd4040abcd34917925cd9c418ac2927cdb5bc
IvanKaziev <99555437+IvanKaziev@users.noreply.github.com>
2023-02-20 12:10:20 +0300

    Update archeogame.py

--- archeogame.py
+++ archeogame.py
@@ -91,7 +91,7 @@ while flag is False:
         save_progress(choice, '0', ability)
 
     # Основной код
-    if reader[choice] not in ['mundare', 'reficere', 'vendere'] and (question == '' or question.lower() == 'да'):
+    if 'sp' not in reader[choice] and (question == '' or question.lower() == 'да'):
         if 'lost' not in reader[choice]:
             for key, val in reader[choice]['choices'].items():
                 print(f'{key} - {val}')
