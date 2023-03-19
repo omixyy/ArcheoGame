287cca6c9d4fe16455faf60fbafc999decaa1eff
IvanKaziev <99555437+IvanKaziev@users.noreply.github.com>
2023-03-19 10:27:45 +0300

    Update archeogame.py

--- archeogame.py
+++ archeogame.py
@@ -150,7 +150,7 @@ while flag is False:
             choice = reader[choice]['choice_change']
             
     # Обнуляем урон храму после успешного использования заклинания
-    if keys_to_json_lvl[int(choice)] == 'sp2':
+    if choice == 'sp2':
         temple_damage = 0
 
     save_progress(choice, temple_damage, ability)
